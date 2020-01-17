98ade47dcbea0030e895dd6f69b3c9e87a04ece6
João Moita Manrique <joaomoitamanrique@gmail.com>
2020-01-16 23:27:51 -0300

    add cors

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const routes = require('./routes')
+const cors = require('cors')
 const app = express()
 
 mongoose.connect('mongodb+srv://omnistack:omnistack@cluster0-yfezx.mongodb.net/week10?retryWrites=true&w=majority', {
@@ -8,6 +9,7 @@ mongoose.connect('mongodb+srv://omnistack:omnistack@cluster0-yfezx.mongodb.net/w
   useUnifiedTopology: true
 })
 
+app.use(cors())
 app.use(express.json())
 app.use(routes)
 
